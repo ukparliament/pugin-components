52f0f6bf857e15d6001359980233b14d5da8365f
Allan Wazacz <cazwazacz@gmail.com>
2018-08-22 16:40:52 +0100

    Update search to optionally have toggle

--- test/unit/components/header.spec.js
+++ test/unit/components/header.spec.js
@@ -1,10 +1,9 @@
 const testHelper = require('../../helpers/test-helper');
 
-
 describe('Header dust component', function() {
-  testHelper.setupBefore()
+  testHelper.setupBefore();
 
   it('should return html from the dust component', function(done) {
-    testHelper.shunterTest('header', 'components__header', 'components', done)
+    testHelper.shunterTest('header', 'components__header', 'components', done);
   });
 });
