354a0f4ab4684e5bb6bfe48fb737f420b634d139
Rebecca Appleyard <rklappleyard@gmail.com>
2018-09-14 11:47:42 +0100

    Added translation to meta title and further testing around page titles

--- test/unit/components/head/meta.spec.js
+++ test/unit/components/head/meta.spec.js
@@ -12,4 +12,22 @@ describe('Head meta dust component', function() {
         testHelper.shunterTest('meta-with-request-id', 'components__head__meta', 'components/head', done)
     });
   });
+
+  context('with a simple title attribute', function() {
+      it('should return html from the dust component including the title given', function(done) {
+          testHelper.shunterTest('meta-with-simple-title', 'components__head__meta', 'components/head', done)
+      });
+  });
+
+  context('with a translation title attribute', function() {
+      it('should return html from the dust component including the translated title', function(done) {
+          testHelper.shunterTest('meta-with-translation-title', 'components__head__meta', 'components/head', done)
+      });
+  });
+
+    context('with no title attribute given', function() {
+        it('should return html from the dust component including the shared title', function(done) {
+            testHelper.shunterTest('meta-with-no-title', 'components__head__meta', 'components/head', done)
+        });
+    });
 });
