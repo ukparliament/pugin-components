4ddd3c81ac3f24f170f8009cc61f69cf0882a864
Allan Wazacz <cazwazacz@gmail.com>
2018-09-19 12:48:58 +0100

    [WEBDEV-716] Add tests for i18next and translation helper This also changes `i18next` from a development dependency to a normal dependency as it was causing issues in Augustus

--- dust/t.js
+++ dust/t.js
@@ -5,17 +5,23 @@ const i18next = require('i18next');
 i18next.init(require('../config/i18next'));
 
 module.exports = initHelper;
-/**
-* This helper allows dust to work with the i18next internationalization package.
-* It passes in parameters which are accessable through the params object, the dust helper then takes in those parameters as its own.
-*/
-function initHelper(dust, renderer, config) {
-	dust.helpers.t = function (chunk, ctx, bodies, params) {
-		let key = dust.helpers.tap(params.key, chunk, ctx)
+module.exports.createTHelper = createTHelper;
 
-		let data = dust.helpers.tap(params.data, chunk, ctx)
-		data = data ? data : {};
+// This helper allows dust to work with the i18next internationalization package.
+// It passes in parameters which are accessable through the params object, the dust helper then takes in those parameters as its own.
+function createTHelper(dustObject) {
+  return function (chunk, ctx, bodies, params) {
+    let key = dustObject.helpers.tap(params.key, chunk, ctx);
 
-		return chunk.write(i18next.t(key, data))
-	};
+    let data = dustObject.helpers.tap(params.data, chunk, ctx);
+    data = data ? data : {};
+
+    return chunk.write(i18next.t(key, data));
+  };
+}
+
+// This function is exported and loaded by Shunter to be used as a helper in dust.
+// It is in a format that Shunter expects, however the last parameter is added so that we can test it.
+function initHelper(dust, paramTwo, paramThree, tHelper = createTHelper) {
+  dust.helpers.t = tHelper(dust);
 }
