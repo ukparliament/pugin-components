55f1f76830f5a4bd679c428e5bff70742e236b75
Peter Hunt <naseberry@users.noreply.github.com>
2018-11-05 15:59:19 +0000

    gtag IP anonymization

--- resources/js/googleAnalytics.js
+++ resources/js/googleAnalytics.js
@@ -5,4 +5,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-15845045-29');
+gtag('config', 'UA-15845045-29', {'anonymize_ip': true});
