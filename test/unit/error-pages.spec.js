cf7b27fe67b3700e97f815d385deb4eee6878eb9
Joshua Waheed <joshua.waheed@gmail.com>
2018-12-12 17:48:37 +0000

    [WEBDEV-1111] Unify footer component in error pages

--- test/unit/error-pages.spec.js
+++ test/unit/error-pages.spec.js
@@ -4,14 +4,14 @@ describe('Error pages', function () {
   testHelper.setupBefore()
 
   it('404 page', (done) => {
-    testHelper.shunterTest('../404-page', 'layout-error-404', 'components', done);
+    testHelper.shunterTest('../layout-error-404', 'layout-error-404', 'components', done);
   });
 
   it('500 page', (done) => {
-    testHelper.shunterTest('../500-page', 'layout-error-500', 'components', done);
+    testHelper.shunterTest('../layout-error-500', 'layout-error-500', 'components', done);
   });
 
   it('502 page', (done) => {
-    testHelper.shunterTest('../502-page', 'layout-error-502', 'components', done);
+    testHelper.shunterTest('../layout-error-502', 'layout-error-502', 'components', done);
   });
 });
